2bf5e0835a9315551bd877bb284ee246e88dd170
Adyllsxn <domingos.nxscimento@gmail.com>
2026-05-19 10:23:40 +0100

    fix: corrige classe text-blue para usar Tailwind - text-blue-600

--- src/apps/Aquasmart.Web/Program.cs
+++ src/apps/Aquasmart.Web/Program.cs
@@ -1,11 +1,13 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Aquasmart.Web;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
