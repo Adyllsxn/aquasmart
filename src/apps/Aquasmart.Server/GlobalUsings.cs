ef742f1114a9feac7aea383dda462bc699659763
Adyllsxn <domingos.nxscimento@gmail.com>
2026-05-20 12:03:57 +0100

    refactor: add global usings for entire project
    - Add global usings for System, Scalar, Microsoft namespaces
    - Add global usings for Domain (Enums, Entities, Exceptions, Validator, Abstractions)
    - Add global usings for Infrastructure (Context, Connection, Services, UoW)
    - Add global usings for Presentation (Configs, Extensions, Pipelines, Hubs, Endpoints)
    - Add global usings for all DTOs, Commands and Queries (Alerta, Comando, LeituraSensor)
    
    Eliminates repetitive using statements across all files
    Improves code readability and maintainability

--- src/apps/Aquasmart.Server/GlobalUsings.cs
+++ src/apps/Aquasmart.Server/GlobalUsings.cs
@@ -1,6 +1,11 @@
+global using System.Net;
+global using Scalar.AspNetCore;
+
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.SignalR;
 
 global using Aquasmart.Server.Source.Domain.Enums;
 global using Aquasmart.Server.Source.Domain.Entities;
@@ -10,3 +15,25 @@ global using Aquasmart.Server.Source.Domain.Shared.Validator;
 global using Aquasmart.Server.Source.Domain.Abstractions.UoW;
 global using Aquasmart.Server.Source.Domain.Abstractions.Services;
 global using Aquasmart.Server.Source.Infrastructure.Data.Context;
+global using Aquasmart.Server.Source.Infrastructure.Data.Connection;
+global using Aquasmart.Server.Source.Infrastructure.Services;
+global using Aquasmart.Server.Source.Infrastructure.UoW;
+global using Aquasmart.Server.Source.Presentation.Common.Configs;
+global using Aquasmart.Server.Source.Presentation.Common.Extensions;
+global using Aquasmart.Server.Source.Presentation.Common.Pipelines;
+global using Aquasmart.Server.Source.Presentation.Hubs;
+global using Aquasmart.Server.Source.Presentation.Enpoints;
+global using Aquasmart.Server.Source.Presentation.Enpoints.System;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Alerta.Commands;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Alerta.Queries;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Alerta.DTOs;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Comando.Commands;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Comando.Queries;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.Comando.DTOs;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.LeituraSensor.Commands;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.LeituraSensor.Queries;
+global using Aquasmart.Server.Source.Presentation.Enpoints.Business.LeituraSensor.DTOs;
+
+
+
+
---
+++ src/apps/Aquasmart.Server/Source/Presentation/Common/Extensions/ModulesExtensions.cs
@@ -0,0 +1,8 @@
+namespace Aquasmart.Server.Source.Presentation.Common.Extensions;
+public static class ModulesExtensions
+{
+    public static void AddModulesExtensions(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddInfrastruture(builder.Configuration);
+    }
+}
